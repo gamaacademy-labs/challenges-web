1380760c5ca859bc5f1d65ee866a308e9b12a884
Israel Silva <israelassissilva13@gmail.com>
2022-08-10 17:24:59 -0300

    feat: typescript interface

--- src/templates/challenges/challenges.template.tsx
+++ src/templates/challenges/challenges.template.tsx
@@ -1,16 +1,27 @@
 import { Box, Typography } from '@gama-academy/smash-web'
 import React from 'react'
+import api from "../../services/challenges.json" 
 
-const ChallengesTemplate = () => {
+interface IChallenges{
+  id: number
+  title: string
+}
+
+interface ChallengesProps{
+  ChallengesTemplate:IChallenges[]
+}
+
+
+export default function ChallengesTemplate ({ChallengesTemplate}:ChallengesProps){
   return (
     <Box dir="row">
-      <Typography type='title' color='secondary.4'>Desafio 1</Typography>
-      <Typography type='title' color='secondary.4'>Desafio 2</Typography>
-      <Typography type='title' color='secondary.4'>Desafio 3</Typography>
-      <Typography type='title' color='secondary.4'>Desafio 4</Typography>
-      <Typography type='title' color='secondary.4'>Desafio 5</Typography>
+      {ChallengesTemplate.map(challenge => {
+        return (
+      <Typography type='title' key={ChallengesTemplate.id}>{ChallengesTemplate.title}</Typography>
+
+        )
+      })}
+      
     </Box>
   )
 }
-
-export default ChallengesTemplate
