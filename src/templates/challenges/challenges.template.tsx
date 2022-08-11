c809506cd5c691cadf3eb87ce3ac9a83498942b8
Adryel Simon <adryel@live.it>
2022-08-11 19:03:04 -0300

    feat: list box component has been styled

--- src/templates/challenges/challenges.template.tsx
+++ src/templates/challenges/challenges.template.tsx
@@ -4,18 +4,25 @@ import { IChallenges } from './index'
 const ChallengesTemplate = ({ challenges }: { challenges: IChallenges[] }) => {
     return (
         <>
-            <Typography align='center'><h1>Desafios</h1></Typography>
-            <ul>
+            <Box display="flex" mb={66} flexDirection="column" alignItems="center">Header</Box>
+
+            <Typography fontFamily={'monospace'} align='center' ><h1>Desafios</h1></Typography>
+
+            <Box alignment='center' margin='lg' dir='column'  >
                 {challenges.map((challenge: IChallenges) => {
                     return (
-                        <Box alignment='center' margin='lg' dir='row' key={challenge.id}>
-                            <Button onClick={() => { }} buttonType='button' size='1'>Iniciar desafio</Button>
-                            <Typography tag='p' align='center'>{challenge.title}
+                        <Box className='hoverEffect' padding={4} borderRadius={'10px'} style={{ width: '75vw' }} border={'1px solid #8d52e5'} justifyContent={'space-between'} alignment={'center'} margin={'lg'} dir={'row'} key={challenge.id}>
+                            <Typography fontFamily={''} tag='p' align='center'>{challenge.title}
                             </Typography>
+                            <Box >
+                                <Button onClick={() => { }} buttonType='button' size='1'>Iniciar desafio</Button>
+                            </Box>
                         </Box>
+
                     )
                 })}
-            </ul>
+            </Box>
+
         </>
     )
 }
