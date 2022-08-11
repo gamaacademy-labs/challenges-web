d5f7e9b9da315279836d8fd7e37ffb9f1d9a49ed
zaida cueto <zcueto58@gmail.com>
2022-08-11 18:08:06 -0300

    feat: header biulding

--- src/components/header/header.comp.tsx
+++ src/components/header/header.comp.tsx
@@ -1,57 +1,48 @@
 import { Box, InputSearch, Icon, Typography } from '@gama-academy/smash-web';
 import Link from 'next/link';
 import React from 'react';
-import Logo from 'img/Logo.png';
 import * as S from './header.styles';
 import Image from 'next/image'
 
 
 export const Header: React.FC = () => {
   return (
-    <S.StyledHeader  >
+    <S.StyledHeader dir="row"  >
       <Box >
-        <Image src="/img/Logo.png" height={30} width={135} />
-
-        {/* <h2>gama academy</h2> */}
-
+        <Image src="/img/Logo.png"  width={120} alt="Logo" height={30} />
       </Box>
       <Box
-
         width={440} height={32}>
-
         <InputSearch
-          // onChangeValue={function noRefCheck() { } }
-          // onClear={function noRefCheck() { } }
-          placeholder="Digite uma palavra-chave" m={undefined} margin={undefined} mt={undefined} marginTop={undefined} mb={undefined} marginBottom={undefined} ml={undefined} marginLeft={undefined} mr={undefined} marginRight={undefined} my={undefined} marginY={undefined} mx={undefined} marginX={undefined}        />
+          placeholder="Digite uma palavra-chave"  
+          onChange={()=>{}}
+              />
       </Box>
 
-      <Box>
-        <S.StyledNav  >
-          <S.StyledNavItem>
+      <Box dir="row">
+          <Box>
             <Link href={"/"}>
               <Typography type='tips'> Início</Typography >
             </Link>
-          </S.StyledNavItem>
+          </Box>
 
-          <S.StyledNavItem>
+          <Box>
             <Typography type='tips'> Carreiras</Typography >
-          </S.StyledNavItem>
+          </Box>
+          <Box>
+            <Icon.Material name="apps" color="secondary.4" size={24} />
 
-          <S.StyledNavItem>
+          </Box>
+          <Box>
 
-            <Icon.Material name="apps" color="secondary.4" size={25} />
+            <Icon.Material name="notifications" color="secondary.4" size={24}  type="round"/>
 
-          </S.StyledNavItem>
-          <S.StyledNavItem>
+          </Box>
 
-            <Icon.Material name="notifications" color="#FFFFFF;" size={25} type="round" shape="round" />
+          <Box>
 
-          </S.StyledNavItem>
-
-          <S.StyledAvatar>
-
-          </S.StyledAvatar>
-        </S.StyledNav >
+          </Box>
+       
       </Box>
 
 
--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -1,28 +1,29 @@
 import styled from 'styled-components';
+import { Box } from '@gama-academy/smash-web';
 
-export const StyledHeader = styled.header`
+export const StyledHeader = styled(Box)`
  border-bottom: 1px solid #e6e6e6;
 display:flex;
 padding: 1rem;
 justify-content: space-between;
 align-items: center;
 `
-export const StyledAvatar = styled.div`
-border-radius: 50%;
-border: 2px solid #000;
-width: 1.5rem;
-height: 1.5rem;
-margin: 1rem ;
-background-color: #aaaa;
-`
+// export const StyledAvatar = styled.div`
+// border-radius: 50%;
+// border: 2px solid #000;
+// width: 1.5rem;
+// height: 1.5rem;
+// margin: 1rem ;
+// background-color: #aaaa;
+// `
 
-export const StyledNavItem = styled.div`
-margin-left: 1rem;
+// export const StyledNavItem = styled.div`
+// margin-left: 1rem;
 
-`
-export const StyledNav = styled.nav`
-align-items: center;
-display: flex;
+// `
+// export const StyledNav = styled.nav`
+// align-items: center;
+// display: flex;
 
 
-`
+// `
