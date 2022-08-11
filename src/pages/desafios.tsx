a557e7c29334ca6b064e12af5dd80d3873bf036d
Adryel Simon <adryel@live.it>
2022-08-11 11:12:51 -0300

    feat: api data fetching, imports, adjusts

--- src/pages/desafios.tsx
+++ src/pages/desafios.tsx
@@ -1,36 +1,12 @@
-//PAGINA ABAIXO (comentado)
-
-// import React from 'react'
-// import ChallengesTemplate from '../templates/challenges/challenges.template'
-
-// const ChallengesPage = () => {
-//     return (
-//         <ChallengesTemplate challenges={[]} />
-//     )
-// }
-// export default ChallengesPage
-
-// ---------------------------------------------------------------
-// ---------------------------------------------------------------
-// ---------------------------------------------------------------
-// ---------------------------------------------------------------
-
-// TEMPLATE ABAIXO (funciona em pages mas nao em tamplates)
-// motivo: erro ao renderizar dados do template em pages
-// consultar se o motivo se deve a algum import ou parametro a ser passado
-
-import { Box, Button, Typography } from '@gama-academy/smash-web'
-import { GetServerSideProps } from 'next'
-import React from 'react'
 import axios from 'axios'
+import ChallengesTemplate from '../templates/challenges/challenges.template'
+import { GetServerSideProps } from 'next'
+import { IChallenges, ChallengesProps } from '../templates/challenges/index'
 
-interface IChallenges {
-    id: number
-    title: string
-}
-
-interface ChallengesProps {
-    challenges: IChallenges[]
+const ChallengesPage = ({ challenges }: ChallengesProps) => {
+    return (
+        <ChallengesTemplate challenges={challenges} />
+    )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -48,25 +24,4 @@ export const getServerSideProps: GetServerSideProps = async () => {
     }
 }
 
-const ChallengesTemplate = ({ challenges }: { challenges: IChallenges[] }) => {
-    return (
-        <>
-            <Typography align='center'><h1>Desafios</h1></Typography>
-            <ul>
-                {challenges.map((challenge: IChallenges) => {
-                    return (
-                        <Box alignment='center' margin='lg' dir='row' key={challenge.id}>
-                            <Button mr="10px" buttonType='button' size='1'>Iniciar desafio</Button>
-                            <Typography tag='p' align='center'>{challenge.title}
-                            </Typography>
-                        </Box>
-                    )
-
-                })}
-            </ul>
-        </>
-
-    )
-}
-
-export default ChallengesTemplate;
+export default ChallengesPage
