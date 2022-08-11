0238dc1d0430dce5acf036332f452f71d743d76f
Adryel Simon <adryel@live.it>
2022-08-11 11:15:26 -0300

    docs: import ts interface and Button Props adjust

--- src/templates/challenges/challenges.template.tsx
+++ src/templates/challenges/challenges.template.tsx
@@ -1,55 +1,23 @@
-// CONTEUDO DISPONIVEL NO ARQUIVO PAGES > DESAFIOS.TSX
+import { Box, Button, Typography } from '@gama-academy/smash-web'
+import { IChallenges } from './index'
 
+const ChallengesTemplate = ({ challenges }: { challenges: IChallenges[] }) => {
+    return (
+        <>
+            <Typography align='center'><h1>Desafios</h1></Typography>
+            <ul>
+                {challenges.map((challenge: IChallenges) => {
+                    return (
+                        <Box alignment='center' margin='lg' dir='row' key={challenge.id}>
+                            <Button onClick={() => { }} buttonType='button' size='1'>Iniciar desafio</Button>
+                            <Typography tag='p' align='center'>{challenge.title}
+                            </Typography>
+                        </Box>
+                    )
+                })}
+            </ul>
+        </>
+    )
+}
 
-// import { Box, Button, Typography } from '@gama-academy/smash-web'
-// import { GetServerSideProps } from 'next'
-// import React from 'react'
-// import axios from 'axios'
-
-// interface IChallenges {
-    //     id: number
-    //     title: string
-    // }
-    
-    // interface ChallengesProps {
-        //     challenges: IChallenges[]
-        // }
-        
-        // export const getServerSideProps: GetServerSideProps = async () => {
-            //     const challengesUrl = 'https://jsonplaceholder.typicode.com/posts'
-            //     const challengesFromApi = await axios.get(challengesUrl)
-            //     const challenges: IChallenges[] = challengesFromApi.data
-            
-            //     if (!challenges) {
-                //         return {
-                    //             notFound: true,
-                    //         };
-                    //     }
-                    //     return {
-//         props: { challenges: challenges },
-//     }
-// }
-
-// const ChallengesTemplate = ({ challenges }: { challenges: IChallenges[] }) => {
-//     return (
-//         <>
-//             <Typography align='center'><h1>Desafios</h1></Typography>
-//             <ul>
-//                 {challenges.map((challenge: IChallenges) => {
-    //                     return (
-        //                         <Box alignment='center' margin='lg' dir='row' key={challenge.id}>
-        //                             <Button mr="10px" buttonType='button' size='1'>Iniciar desafio</Button>
-        //                             <Typography tag='p' align='center'>{challenge.title}
-        //                             </Typography>
-        //                         </Box>
-        //                     )
-        //                 })}
-        //             </ul>
-        //         </>
-        
-        //     )
-        // }
-        
-        // export default ChallengesTemplate;
-        
-        export {} // parar erro 'isolatedModules'
+export default ChallengesTemplate;
