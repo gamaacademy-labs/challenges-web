9bc769983f3f6617c1cfd9bd953c0e42000d3a16
Adryel Simon <adryel@live.it>
2022-08-12 18:49:27 -0300

    refactor: container and header component refactoring in globalscss

--- src/templates/challenges/challenges.template.tsx
+++ src/templates/challenges/challenges.template.tsx
@@ -1,30 +1,43 @@
-import { Box, Button, Typography } from '@gama-academy/smash-web'
-import { IChallenges } from './index'
+import { Box, Button, Typography } from "@gama-academy/smash-web";
+import { Header } from "../../components/header";
+import { IChallenges } from "./index";
 
 const ChallengesTemplate = ({ challenges }: { challenges: IChallenges[] }) => {
-    return (
-        <>
-            <Box display="flex" mb={66} flexDirection="column" alignItems="center">Header</Box>
-
-            <Typography fontFamily={'monospace'} align='center' ><h1>Desafios</h1></Typography>
-
-            <Box alignment='center' margin='lg' dir='column'  >
-                {challenges.map((challenge: IChallenges) => {
-                    return (
-                        <Box className='hoverEffect' padding={4} borderRadius={'10px'} style={{ width: '75vw' }} border={'1px solid #8d52e5'} justifyContent={'space-between'} alignment={'center'} margin={'lg'} dir={'row'} key={challenge.id}>
-                            <Typography fontFamily={''} tag='p' align='center'>{challenge.title}
-                            </Typography>
-                            <Box >
-                                <Button onClick={() => { }} buttonType='button' size='1'>Iniciar desafio</Button>
-                            </Box>
-                        </Box>
-
-                    )
-                })}
+  return (
+    <>
+      <Header />
+      <Typography fontFamily={"monospace"} align="center">
+        <h1>Desafios</h1>
+      </Typography>
+      <Box alignment="center" margin="lg" dir="column">
+        {challenges.map((challenge: IChallenges) => {
+          return (
+            <Box
+              className="hoverEffect"
+              width="75vw"
+              padding="4"
+              borderRadius="10px"
+              border={"1px solid #D7DBDA"}
+              justifyContent={"space-between"}
+              alignment={"center"}
+              margin={4}
+              dir={"row"}
+              key={challenge.id}
+            >
+              <Typography fontFamily={"raniway"} tag="p" align="center">
+                {challenge.title}
+              </Typography>
+              <Box>
+                <Button onClick={() => {}} buttonType="button" size="1">
+                  Iniciar desafio
+                </Button>
+              </Box>
             </Box>
-
-        </>
-    )
-}
+          );
+        })}
+      </Box>
+    </>
+  );
+};
 
 export default ChallengesTemplate;
