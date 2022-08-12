e5cd9e50e555690bac522c22bbd4f2de94b6ad3d
Israel Silva <israelassissilva13@gmail.com>
2022-08-12 12:33:16 -0300

    feat: header in 'desafios' page

--- src/components/header/header.comp.tsx
+++ src/components/header/header.comp.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 
 export const Header: React.FC = () => {
   return (
-    <S.StyledHeader dir="row" width="100%" alignItems="center" justifyContent="space-between" padding="4" mb="1">
+    <S.StyledHeader dir="row" width="100%" alignItems="center" justifyContent="space-between" padding="4" mb="2" border='1px solid gray' >
       <Box >
         <Image src="/img/Logo.png" width={120} alt="Logo" height={30} />
       </Box>
@@ -29,17 +29,17 @@ export const Header: React.FC = () => {
         <Box ml="2">
           <Typography type='tips'> Carreiras</Typography >
         </Box>
-        <Box ml="2">
-          <Icon.Material name="apps" color="secondary.4" size={24} />
+        <Box ml="2" >
+          <Icon.Material name="apps" color="secondary.4" size={32} />
 
         </Box>
-        <Box ml="2">
+        <Box ml="2" backgroundColor={'secondary.4'} borderRadius="50%" size={24} display="flex" justifyContent="center">
 
-          <Icon.Material name="notifications" color="secondary.4" size={24} type="round" />
+          <Icon.Material name="notifications" color="white"size={16} type="round" />
 
         </Box>
-        <S.StyledAvatar ml="2">
-
+        <S.StyledAvatar ml="2" size={32} backgroundColor='black' borderRadius="50%">
+          
         </S.StyledAvatar>
       </Box>
 
--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -6,10 +6,7 @@ export const StyledHeader = styled(Box)`
 
 `
 export const StyledAvatar = styled(Box)`
-border-radius: 50%;
-width: 25px;
-height: 25px;
-background-color: #000;
+
 `
 
 // export const StyledNavItem = styled.div`
