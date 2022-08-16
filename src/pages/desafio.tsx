a3afb24fca08f4c474c6aa5942f97e575661e19f
Adryel Simon <adryel@live.it>
2022-08-15 22:33:32 -0300

    feat: setup page 'desafio'

---
+++ src/pages/desafio.tsx
@@ -0,0 +1,13 @@
+import { ChallengeTemplate } from '../templates/challenge/challenge.template';
+
+
+const ChallengesPage = () => {
+    return (
+        <>
+        <ChallengeTemplate/>
+        </>
+     
+    )
+}
+
+export default ChallengesPage;
