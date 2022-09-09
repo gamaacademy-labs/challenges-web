f513c8d959d8148bbbb382eefc97e37ddc8fd84c
Nikolas Roger <boonretsam@gmail.com>
2022-09-09 09:57:54 -0300

    fix: use dimensions hook

--- src/hooks/layout/use-dimensions.hook.ts
+++ src/hooks/layout/use-dimensions.hook.ts
@@ -1,41 +1,26 @@
-import * as React from 'react'
+import { useEffect, useState } from 'react';
 
-import { useIsomorphicLayoutEffect } from './use-isomorphic-layout-effect.hook'
+type WindowDimentions = {
+    width: number;
+    height: number;
+};
 
-import { UseDimensionsResponse } from './use-dimensions.types'
+export const useDimensions = (): WindowDimentions => {
+    const [windowDimensions, setWindowDimensions] = useState<WindowDimentions>({
+        width: 0,
+        height: 0,
+    });
+    useEffect(() => {
+        function handleResize(): void {
+            setWindowDimensions({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return (): void => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
 
-const getWindowDimensions = (): UseDimensionsResponse => {
-  if (typeof window === 'undefined') return { width: 0, height: 0 }
-  const { innerWidth: width, innerHeight: height } = window
-  return { width, height }
-}
-
-/**
- * This is just an alternative to use the useLayoutEffect without catching the gigantic warning
- *
- * Credits to: https://medium.com/@alexandereardon/uselayouteffect-and-ssr-192986cdcf7a
- */
-
-export const useDimensions = (): UseDimensionsResponse => {
-  const [windowDimensions, setWindowDimensions] = React.useState<UseDimensionsResponse>(getWindowDimensions())
-
-  function handleResize() {
-    setWindowDimensions(getWindowDimensions())
-  }
-
-  React.useEffect(() => {
-    window.addEventListener('resize', handleResize)
-    window.addEventListener('load', handleResize)
-
-    return () => {
-      window.removeEventListener('load', handleResize)
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-
-  useIsomorphicLayoutEffect(() => {
-    handleResize()
-  }, [])
-
-  return windowDimensions
-}
+    return windowDimensions;
+};
