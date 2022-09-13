e2621defe28ff4d1685901574d461bb0f8e214c2
zaida cueto <zcueto58@gmail.com>
2022-09-13 14:32:14 -0300

    feat: modal de gabarito e parabéns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
+import { Button } from '@gama-academy/smash-web';
 import type { NextPage } from 'next'
+import { useState } from 'react';
 import { Header } from '../components/header/header.comp';
+import { CongratulationModal } from '../templates/start-challenge/components/congratulation-modal/congratulation-modal.comp';
+import { GabaritoModal } from '../templates/start-challenge/components/gabarito-modal /gabarito-modal.comp';
 
 const Home: NextPage = () => {
+  const [congratulationModalVisible, setCongratulationModal] = useState(false)
   return (
     <div>
       <Header/>
-      <h1>
-        ...
-      </h1>
+      <Button
+    onClick={ () =>setCongratulationModal(true)}
+      >
+Congratulation Modal
+      </Button>
+<CongratulationModal visible={congratulationModalVisible} close={()=>  setCongratulationModal(false)}/>
+      <Button
+    onClick={ () =>setCongratulationModal(true)}
+      >
+Congratulation Modal
+      </Button>
+<GabaritoModal visible={congratulationModalVisible} close={()=>  setCongratulationModal(false)}/>
+
     </div>
   )
 }
