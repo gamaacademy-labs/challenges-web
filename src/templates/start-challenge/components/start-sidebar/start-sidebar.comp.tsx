983e77fce042e06ff69cffabf226360577ea89d0
zaida cueto <zcueto58@gmail.com>
2022-09-13 15:34:52 -0300

    feat:feedback

--- src/templates/start-challenge/components/start-sidebar/start-sidebar.comp.tsx
+++ src/templates/start-challenge/components/start-sidebar/start-sidebar.comp.tsx
@@ -7,6 +7,7 @@ import { useDimensions } from '../../../../hooks/layout/use-dimensions.hook';
 import { MOBILE_BREAKPOINT } from '../../../../utils/responsive';
 
 import StartMaterialsTab from '../start-materials-tab/start-materials-tab.comp';
+import Feedback from '../feedback/feedback.comp';
 
 const StartSidebar = () => {
 
@@ -29,6 +30,7 @@ const StartSidebar = () => {
 
     return (
         <Box width={width > MOBILE_BREAKPOINT ? 300 : "100%"} margin="2" padding="2">
+
             <Box
                 borderColor="primary.3"
                 borderStyle="solid"
@@ -38,6 +40,8 @@ const StartSidebar = () => {
             >
                 <Image src="/img/image.png" width={300} alt="Logo" height={200} />
             </Box>
+        <Box pb="5" > <Feedback/> </Box>
+        
             {
                 width <= MOBILE_BREAKPOINT && (
                     <Box alignItems="flex-start">
