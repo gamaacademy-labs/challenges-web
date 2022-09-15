ca60a8fedb82abdf1c9bf3866457301bd18f5cdb
zaida cueto <zcueto58@gmail.com>
2022-09-15 10:51:45 -0300

    fix: deleted code not used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,12 @@
-import { Box, Button, Typography } from '@gama-academy/smash-web';
 import type { NextPage } from 'next'
-import { useState } from 'react';
 import { Header } from '../components/header/header.comp';
-import { TimeUpModal } from '../templates/start-challenge/components/modal-Time-up/modal-time-up.comp';
+
 
 const Home: NextPage = () => {
-  const [orientationModalVisible, setOrientationModalVisible] = useState(false)
+
   return (
     <div>
       <Header/>
-
-
-  
-
-   
-        <div></div>
-
-        <Button
-            color="white"
-            onClick={ () => setOrientationModalVisible(true)}
-        
-        >
-     
-
-          buttom modal 
-        </Button>
-
-<TimeUpModal visible={orientationModalVisible} close={()=> setOrientationModalVisible(false)}  />
       <h1>
         ...
       </h1>
