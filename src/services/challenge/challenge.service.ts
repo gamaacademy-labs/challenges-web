3c80cca2153c84bfa78b56104e8654bf81551781
Nikolas Roger <boonretsam@gmail.com>
2022-09-15 17:57:13 -0300

    fix: https

--- src/services/challenge/challenge.service.ts
+++ src/services/challenge/challenge.service.ts
@@ -3,7 +3,7 @@ import { Challenge } from "../challenges/challenges.types";
 
 const getChallengeById = async (challengeId: string): Promise<Challenge> => {
   const response = await axios.get(
-    `http://18.231.74.183/challenges/${challengeId}`
+    `https://18.231.74.183/challenges/${challengeId}`
   );
   return response.data;
 };
--- src/services/challenges/challenges.service.ts
+++ src/services/challenges/challenges.service.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import { GetChallengesResponse, Material } from "./challenges.types";
 
 const getChallenges = async (): Promise<GetChallengesResponse> => {
-  const response = await axios.get("http://18.231.74.183/challenges");
+  const response = await axios.get("https://18.231.74.183/challenges");
   return response.data;
 };
 const getMaterials = async (challengeId: string): Promise<Material[]> => {
   const response = await axios.get(
-    `http://18.231.74.183/materials/${challengeId}`
+    `https://18.231.74.183/materials/${challengeId}`
   );
   return response.data;
 };
